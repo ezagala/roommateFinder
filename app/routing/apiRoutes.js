d5542413322c35b68dfea8d9427ac94ae783be50
Eric <ejzagala@gmail.com>
2018-05-07 00:16:33 -0600

    Commity

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,16 +5,46 @@ module.exports = function(app) {
     app.get("/api/roommates", (req, res) => {
         res.json(roommates); 
 
-        console.log(roommates);
+        
 
     })
 
-    // app.post("/api/roommates", (req, res) => {
-    //     const roommate = req.body; 
+    app.put("/api/roommates", (req, res) => {
+        const roommate = req.body; 
+        console.log(roommate); 
 
+        const comparisons = []; 
 
+        roommates.forEach(v => {
+            v.scores.forEach(w => {
+                roommate.scores.forEach(x => {
 
-    //     res.json(roommate);
-    // })
+                    const differences = []; 
 
+                    if (w > x) {
+                        differences.push(w - x)
+                    } else if (x > w) {
+                        differences.push(x - w)
+                    } else {
+                        differences.push(0); 
+                    }
+
+                    const matchScore = differences.reduce(getSum) 
+                    comparisons.push({name: v.name, matchScore: matchScore})
+
+                }); 
+            }); 
+         })
+
+        // Add the new profile to the data
+        roommates.push(roommate); 
+        //Send the match back to the client 
+        res.json(match);
+    })
+
+}
+
+// Helper 
+function getSum(total, num) {
+    return total + num;
 }
